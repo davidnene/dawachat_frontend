bad6243d400a84361ba3e6e3f2c99c9a0e7bbdc2
David Nene <64870164+davidnene@users.noreply.github.com>
2024-11-06 00:03:37 +0300

    Configure query-dosage POST body

--- src/services/queryService.js
+++ src/services/queryService.js
@@ -1,6 +1,6 @@
 import api from './api';
 
 export const queryDosage = async (query) => {
-    const response = await api.get(`/api/query-dosage?query=${query}`);
-    return response.data.response;
+    const response = await api.post('/api/query-dosage', {"query": `${query}`});
+    return response.data.response.content;
 };
