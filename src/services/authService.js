c3c67158936858a69bd2df19fb43e92314a8c27f
David Nene <64870164+davidnene@users.noreply.github.com>
2024-11-06 00:04:19 +0300

    Store token in local storage

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,5 +2,10 @@ import api from './api';
 
 export const loginUser = async (email, password) => {
     const response = await api.post('/api/login', { email, password });
+    const { access_token } = response.data;
+    
+    // Store the token in localStorage
+    localStorage.setItem('token', access_token);
+    
     return response.data;
 };
