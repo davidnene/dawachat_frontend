5700ee60c385c038c1cdafa54bf1450df685cb03
David Nene <64870164+davidnene@users.noreply.github.com>
2024-11-06 00:02:55 +0300

    Configure token in localstorage

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,30 @@
+// src/services/api.js
 import axios from 'axios';
 
+// Create an Axios instance
 const api = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
 });
+
+// Add a request interceptor to include the token in the headers
+api.interceptors.request.use(
+    (config) => {
+        // Retrieve the token from localStorage
+        const token = localStorage.getItem('token');
+        
+        // If the token exists, add it to the Authorization header
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        // Handle request error
+        return Promise.reject(error);
+    }
+);
 
 export default api;
