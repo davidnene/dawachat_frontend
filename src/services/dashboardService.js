12c4f1e1b2281eb3b852d07fff5f3cb49aaf877c
David Nene <64870164+davidnene@users.noreply.github.com>
2025-01-28 11:40:55 +0300

    Add dashboard service

---
+++ src/services/dashboardService.js
@@ -0,0 +1,8 @@
+// import api from "./api";
+import dashboardData from '../data.json'
+
+export const fetchDashboardData = async () => {
+//   const response = await api.get("/api/dashboard-data");
+//   return response.data;
+    return dashboardData
+};
