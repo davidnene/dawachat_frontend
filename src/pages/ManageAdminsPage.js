0f368bb4badf83d133619a16b662f83a97d28d32
David Nene <64870164+davidnene@users.noreply.github.com>
2025-02-05 17:19:28 +0300

    Add template for super_admin module

---
+++ src/pages/ManageAdminsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageAdminsPage = () => {
+    
+    return(
+        <div>Manage Admins Page</div>
+    );
+
+
+};
+
+export default ManageAdminsPage;
---
+++ src/pages/ManageHospitalsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageHospitalsPage = () => {
+    
+    return(
+        <div>Manage Hospitals Page</div>
+    );
+
+
+};
+
+export default ManageHospitalsPage;
