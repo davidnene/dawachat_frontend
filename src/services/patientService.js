9f29a9dc99f522fd3d6c3f49beaa5642c1222c1f
David Nene <64870164+davidnene@users.noreply.github.com>
2025-02-10 14:42:43 +0300

    Configure patient management by admin

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,5 +1,9 @@
 import api from "./api";
 
+export const createPatient = async (patientData) => {
+  return await api.post("/api/create-patient/", patientData);
+};
+
 export const getPatients = async () => {
   const response = await api.get("/api/patients/");
   return response.data;
@@ -7,4 +11,8 @@ export const getPatients = async () => {
 
 export const updatePatient = async (patientId, patientData) => {
   return await api.put(`/api/update-patient/${patientId}`, patientData);
+};
+
+export const deletePatient = async (patientId, patientData) => {
+  return await api.delete(`/api/delete-patient/${patientId}`, patientData);
 };
