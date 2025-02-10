118c3cbdba5e0cb6793cbfd9f00a81cd5c853e33
David Nene <64870164+davidnene@users.noreply.github.com>
2025-02-10 12:47:21 +0300

    Configure default root after token expiry

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -5,7 +5,7 @@ import AuthContext from '../context/AuthContext';
 const PrivateRoute = ({ children }) => {
     const { user } = useContext(AuthContext);
 
-    return user ? children : <Navigate to="/dashboard" />;
+    return user ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
