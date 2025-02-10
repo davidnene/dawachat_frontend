d514a96a0b8721e03ca9385a6d2d3a281c7270fe
David Nene <64870164+davidnene@users.noreply.github.com>
2025-02-10 12:47:46 +0300

    Configure DoctorManagement Component positioning

--- src/pages/ManageDoctorsPage.js
+++ src/pages/ManageDoctorsPage.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import DoctorManagement from '../components/DoctorManagement'
+import React from "react";
+import DoctorManagement from "../components/DoctorManagement";
+import { Box } from "@mui/material";
 
 const ManageDoctorsPage = () => {
-    
-    return(
-        <DoctorManagement></DoctorManagement>
-    );
-
-
+  return (
+    <Box sx={{ ml: { xs: 0, md: 5 }, p: 3 }}> 
+      <DoctorManagement />
+    </Box>
+  );
 };
 
 export default ManageDoctorsPage;
